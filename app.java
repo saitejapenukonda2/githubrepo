00f0b073b579bb66c7532b6b180cab119583ab19
saitejapenukonda2 <saitejapenukonda2@gmail.com>
2025-11-15 17:37:33 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,5 +1,10 @@
 i need to test the developer process
 need to add more herp
 3457  
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // This line prints "Hello, World!" to the console
+    }
+}
 
 
